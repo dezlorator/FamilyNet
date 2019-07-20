3dbd7a4b1b18be46783436f89122b52ac8187b88
Karina Belikova <kiit15.belikova@gmail.com>
2019-07-20 11:00:41 +0300

    edit button

---
+++ FamilyNet/FamilyNet/wwwroot/js/uploading.js
@@ -0,0 +1,25 @@
+﻿// Copyright © 2017 Dmitry Sikorsky. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+function uploadFiles(inputId) {
+    var input = document.getElementById(inputId);
+    var files = input.files;
+    var formData = new FormData();
+
+    for (var i = 0; i != files.length; i++) {
+        formData.append("files", files[i]);
+    }
+
+    $.ajax(
+        {
+            url: "/uploader",
+            data: formData,
+            processData: false,
+            contentType: false,
+            type: "POST",
+            success: function (data) {
+                alert("Files Uploaded!");
+            }
+        }
+    );
+}
