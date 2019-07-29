969cb3e64c314b9b5b79a351410b9c924c00af28
PavelTcsiposchuk <48702275+PavelTcsiposchuk@users.noreply.github.com>
2019-07-29 22:24:34 +0300

    added migration(delete child in baseitemtype)

---
+++ FamilyNet/FamilyNet/Migrations/20190729192407_Delete child in BaseItemType.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FamilyNet.Migrations
+{
+    public partial class DeletechildinBaseItemType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BaseItemType_BaseItemType_ChildID",
+                table: "BaseItemType");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BaseItemType_ChildID",
+                table: "BaseItemType");
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Donations",
+                newName: "LastDateWhenStatusChanged");
+
+            migrationBuilder.RenameColumn(
+                name: "ChildID",
+                table: "BaseItemType",
+                newName: "ParentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BaseItemType_ParentID",
+                table: "BaseItemType",
+                column: "ParentID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BaseItemType_BaseItemType_ParentID",
+                table: "BaseItemType",
+                column: "ParentID",
+                principalTable: "BaseItemType",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BaseItemType_BaseItemType_ParentID",
+                table: "BaseItemType");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BaseItemType_ParentID",
+                table: "BaseItemType");
+
+            migrationBuilder.RenameColumn(
+                name: "LastDateWhenStatusChanged",
+                table: "Donations",
+                newName: "Date");
+
+            migrationBuilder.RenameColumn(
+                name: "ParentID",
+                table: "BaseItemType",
+                newName: "ChildID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BaseItemType_ChildID",
+                table: "BaseItemType",
+                column: "ChildID",
+                unique: true,
+                filter: "[ChildID] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BaseItemType_BaseItemType_ChildID",
+                table: "BaseItemType",
+                column: "ChildID",
+                principalTable: "BaseItemType",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
