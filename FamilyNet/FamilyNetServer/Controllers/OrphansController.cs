cda59b99eac24598d27a60571c4d48047d9b7739
Irina <cg.irenecooper@gmail.com>
2019-10-10 16:39:14 +0300

    add api controller children

--- FamilyNet/FamilyNetServer/Controllers/OrphansController.cs
+++ FamilyNet/FamilyNetServer/Controllers/OrphansController.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using FamilyNetServer.Models;
-using FamilyNetServer.Models.EntityFramework;
 using FamilyNetServer.Models.Interfaces;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using System.IO;
 using Microsoft.AspNetCore.Authorization;
 using FamilyNetServer.Models.ViewModels;
 using FamilyNetServer.Infrastructure;
---
+++ FamilyNet/FamilyNetServer/FileUploaders/FileUploader.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace FamilyNetServer.FileUploaders
+{
+    public class FileUploader : IFileUploader
+    {
+        private readonly IHostingEnvironment _environment;
+
+        public FileUploader(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public string CopyFile(string fileName, string directory, IFormFile file)
+        {
+            var webRoot = _environment.WebRootPath;
+            var extention = Path.GetExtension(file.FileName);
+
+            var filePath = Path.Combine(webRoot, directory, fileName) + extention;
+
+            if (file.Length > 0)
+            {
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+            }
+
+            return Path.Combine(directory, fileName) + extention;
+        }
+    }
+}
