c760072acb23f6643115d86f6ca5f2c87b2132ed
Irina <cg.irenecooper@gmail.com>
2019-10-18 09:13:02 +0300

    add roles and admins controllers documentation

--- FamilyNet/DoxyGen/html/search/all_f.js
+++ FamilyNet/DoxyGen/html/search/all_f.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['the_20mit_20license_20_28mit_29_130',['The MIT License (MIT)',['../md__c_1__users_alter__one_drive__xD0_xA0_xD0_xB0_xD0_xB1_xD0_xBE_xD1_x87_xD0_xB8_xD0_xB9__xD1_x883d1600b6c473489c1e841a00bd203c6.html',1,'']]],
+  ['the_20mit_20license_20_28mit_29_130',['The MIT License (MIT)',['../md__d_1_ss__family_net2__family_net__family_net__family_net_wwwroot_lib_jquery-validation__l_i_c_e_n_s_e.html',1,'']]],
   ['table_131',['Table',['../class_family_net_1_1_controllers_1_1_charity_makers_controller.html#a7c9c8c407b065d5bc302103c10d53aeb',1,'FamilyNet::Controllers::CharityMakersController']]],
   ['test_132',['test',['../class_family_net_1_1_migrations_1_1test.html',1,'FamilyNet::Migrations']]],
   ['typebaseitem_133',['TypeBaseItem',['../class_family_net_1_1_models_1_1_type_base_item.html',1,'FamilyNet::Models']]]
--- FamilyNet/DoxyGen/html/search/pages_0.js
+++ FamilyNet/DoxyGen/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['the_20mit_20license_20_28mit_29_397',['The MIT License (MIT)',['../md__c_1__users_alter__one_drive__xD0_xA0_xD0_xB0_xD0_xB1_xD0_xBE_xD1_x87_xD0_xB8_xD0_xB9__xD1_x883d1600b6c473489c1e841a00bd203c6.html',1,'']]]
+  ['the_20mit_20license_20_28mit_29_397',['The MIT License (MIT)',['../md__d_1_ss__family_net2__family_net__family_net__family_net_wwwroot_lib_jquery-validation__l_i_c_e_n_s_e.html',1,'']]]
 ];
