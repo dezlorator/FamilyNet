741cfdfceedb71584dfc556b5e2824502739e1ce
Irina <cg.irenecooper@gmail.com>
2019-10-26 20:35:16 +0300

    add lib NUnit, Moq

---
+++ FamilyNet/FamilyNetServer.Tests/ChildValidatorTest.cs
@@ -0,0 +1,27 @@
+﻿using DataTransferObjects;
+using FamilyNetServer.Validators;
+using NUnit.Framework;
+
+namespace FamilyNetServer.Tests
+{
+    class ChildValidatorTest
+    {
+        [Test]
+        public void ChildValidator_WithNullChildDTO_ReturnFalse()
+        {
+            var validator = new ChildValidator();
+
+            Assert.False(validator.IsValid(null));
+        }
+
+        [Test]
+        public void ChildValidator_WithEmptyChildDTO_ReturnFalse()
+        {
+            var childDTO = new ChildDTO();
+
+            var validator = new ChildValidator();
+
+            Assert.False(validator.IsValid(childDTO));
+        }
+    }
+}
