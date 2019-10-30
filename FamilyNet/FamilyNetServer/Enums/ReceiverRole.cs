3540888856ee4999b225e3deb42a2c85e7274d81
dezlorator <44552247+dezlorator@users.noreply.github.com>
2019-10-30 23:06:57 +0200

    FeedData model added

---
+++ FamilyNet/FamilyNetServer/Enums/ReceiverRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyNetServer.Enums
+{
+    public enum ReceiverRole
+    {
+        CharityMaker = 1,
+        Volunteer,
+        Child,
+        Representative
+    }
+}
---
+++ FamilyNet/FamilyNetServer/Models/Feedback.cs
@@ -0,0 +1,23 @@
+﻿using FamilyNetServer.Enums;
+using FamilyNetServer.Models.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FamilyNetServer.Models
+{
+    public class Feedback : IEntity
+    {
+        public int ID { get; set; }
+        [Display(Name = "Feedback message")]
+        public string Message { get; set; }
+        public IFormFile Image { get; set; }
+        public int DonationId { get; set; }
+        [Display(Name = "Receiver role")]
+        public ReceiverRole ReceiverRole { get; set; }
+        [Display(Name = "Rating")]
+        public double Rating { get; set; } 
+        public bool IsDeleted { get; set; }
+    }
+}
