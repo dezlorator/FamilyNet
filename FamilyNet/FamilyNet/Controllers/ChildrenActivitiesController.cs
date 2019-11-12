3cde7e5fbc28fa2e2da331ee74bf3cf43c875a4d
LMichael <lmichael1003@gmail.com>
2019-11-12 13:49:15 +0200

    children activities client controller added

---
+++ FamilyNet/FamilyNet/Controllers/ChildrenActivitiesController.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FamilyNet.Models;
+using FamilyNet.Models.ViewModels;
+using DataTransferObjects;
+using FamilyNet.Downloader;
+using Microsoft.Extensions.Localization;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net;
+using FamilyNet.IdentityHelpers;
+
+namespace FamilyNet.Controllers
+{
+    public class ChildrenActivitiesController : Controller
+    {
+        #region private fields
+
+        private readonly IIdentityInformationExtractor _identityInformationExtactor;
+        private readonly ServerSimpleDataDownloader<ChildActivityDTO> _childrenActivitiesDownloader;
+        private readonly IURLChildrenActivitesBuilder _URLChildrenActivitiesBuilder;
+        private readonly string _apiChildrenActivitiesPath = "api/v2/childrenActivities";
+        private readonly IStringLocalizer<ChildrenActivitiesController> _localizer;
+
+        #endregion
+
+        #region ctor
+
+        public ChildrenActivitiesController(ServerSimpleDataDownloader<ChildActivityDTO> childrenActivitiesDownloader,
+                                 IURLChildrenActivitesBuilder URLChildrenActivitiesBuilder,
+                                 IIdentityInformationExtractor identityInformationExtactor,
+                                 IStringLocalizer<ChildrenActivitiesController> localizer)
+        {
+            _childrenActivitiesDownloader = childrenActivitiesDownloader;
+            _URLChildrenActivitiesBuilder = URLChildrenActivitiesBuilder;
+            _identityInformationExtactor = identityInformationExtactor;
+            _localizer = localizer;
+        }
+
+        #endregion
+
+        public async Task<IActionResult> Index(ChildActivitySearchModel searchModel)
+        {
+            var url = _URLChildrenActivitiesBuilder.GetAllWithFilter(_apiChildrenActivitiesPath,
+                                                                     searchModel);
+            IEnumerable<ChildActivityDTO> childrenActivities = null;
+
+            try
+            {
+                childrenActivities = await _childrenActivitiesDownloader.GetAllAsync(url, HttpContext.Session);
+            }
+            catch (ArgumentNullException)
+            {
+                return Redirect("/Home/Error");
+            }
+            catch (HttpRequestException)
+            {
+                return Redirect("/Home/Error");
+            }
+            catch (JsonException)
+            {
+                return Redirect("/Home/Error");
+            }
+
+            GetViewData();
+
+            return View(childrenActivities);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return Redirect("/Orphans/Index");
+            }
+
+            var url = _URLChildrenActivitiesBuilder.GetById(_apiChildrenActivitiesPath, id.Value);
+            ChildActivityDTO ChildActivityDTO = null;
+
+            try
+            {
+                ChildActivityDTO = await _childrenActivitiesDownloader.GetByIdAsync(url, HttpContext.Session);
+            }
+            catch (ArgumentNullException)
+            {
+                return Redirect("/Home/Error");
+            }
+            catch (HttpRequestException)
+            {
+                return Redirect("/Home/Error");
+            }
+            catch (JsonException)
+            {
+                return Redirect("/Home/Error");
+            }
+
+            if (ChildActivityDTO == null)
+            {
+                return NotFound();
+            }
+
+            GetViewData();
+
+            return View(ChildActivityDTO);
+        }
+
+        public IActionResult Create()
+        {
+            GetViewData();
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ChildActivityDTO childActivityDTO)
+        {
+            var url = _URLChildrenActivitiesBuilder.CreatePost(_apiChildrenActivitiesPath);
+            var message = await _childrenActivitiesDownloader.CreatePostAsync(url, childActivityDTO,
+                                                                 HttpContext.Session);
+
+            if (message.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                return Redirect("/Account/Login");
+            }
+
+            if (message.StatusCode != HttpStatusCode.Created)
+            {
+                return Redirect("/Home/Error");
+            }
+
+            GetViewData();
+
+            return Redirect("/Orphans/Index");
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var urlChildActivity = _URLChildrenActivitiesBuilder.GetById(_apiChildrenActivitiesPath, id.Value);
+
+            ChildActivityDTO childActivityDTO = null;
+
+            try
+            {
+                childActivityDTO = await _childrenActivitiesDownloader.GetByIdAsync(urlChildActivity, HttpContext.Session);
+            }
+            catch (ArgumentNullException)
+            {
+                return Redirect("/Home/Error");
+            }
+            catch (HttpRequestException)
+            {
+                return Redirect("/Home/Error");
+            }
+            catch (JsonException)
+            {
+                return Redirect("/Home/Error");
+            }
+
+            GetViewData();
+
+            return View(childActivityDTO);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ChildActivityDTO ChildActivityDTO)
+        {
+            if (id != ChildActivityDTO.ID)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(ChildActivityDTO);
+            }
+
+            var urlChildren = _URLChildrenActivitiesBuilder.GetById(_apiChildrenActivitiesPath, id);
+
+            var message = await _childrenActivitiesDownloader.CreatePutAsync(urlChildren, ChildActivityDTO,
+                                              HttpContext.Session);
+
+            if (message.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                return Redirect("/Account/Login");
+            }
+
+            if (message.StatusCode != HttpStatusCode.NoContent)
+            {
+                return Redirect("/Home/Error");
+            }
+
+            GetViewData();
+
+            return Redirect("/Orphans/Index");
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var url = _URLChildrenActivitiesBuilder.GetById(_apiChildrenActivitiesPath, id.Value);
+            ChildActivityDTO ChildActivityDTO = null;
+
+            try
+            {
+                ChildActivityDTO = await _childrenActivitiesDownloader.GetByIdAsync(url, HttpContext.Session);
+            }
+            catch (ArgumentNullException)
+            {
+                return Redirect("/Home/Error");
+            }
+            catch (HttpRequestException)
+            {
+                return Redirect("/Home/Error");
+            }
+            catch (JsonException)
+            {
+                return Redirect("/Home/Error");
+            }
+
+            if (ChildActivityDTO == null)
+            {
+                return NotFound();
+            }
+
+            GetViewData();
+
+            return View(id.Value);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (id <= 0)
+            {
+                return NotFound();
+            }
+
+            var url = _URLChildrenActivitiesBuilder.GetById(_apiChildrenActivitiesPath, id);
+
+            var message = await _childrenActivitiesDownloader.DeleteAsync(url, HttpContext.Session);
+
+            if (message.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                return Redirect("/Account/Login");
+            }
+
+            if (message.StatusCode != HttpStatusCode.NoContent)
+            {
+                return Redirect("/Home/Error");
+            }
+
+            GetViewData();
+
+            return Redirect("/Orphans/Index");
+        }
+
+        private void GetViewData()
+        {
+            ViewData["ChildrenActivities"] = _localizer["ChildrenActivities"];
+
+            _identityInformationExtactor.GetUserInformation(HttpContext.Session,
+                                                                 ViewData);
+        }
+    }
+}
---
+++ FamilyNet/FamilyNet/Downloader/ServerChildrenActivitiesDownloader.cs
@@ -0,0 +1,81 @@
+﻿using DataTransferObjects;
+using FamilyNet.HttpHandlers;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FamilyNet.Downloader
+{
+    public class ServerChildrenActivitiesDownloader : ServerSimpleDataDownloader<ChildActivityDTO>
+    {
+        public ServerChildrenActivitiesDownloader(IHttpAuthorizationHandler authorizationHandler)
+            : base(authorizationHandler) { }
+
+        public override async Task<HttpResponseMessage> CreatePostAsync(string url,
+                                                            ChildActivityDTO dto,
+                                                            ISession session)
+        {
+            HttpResponseMessage msg = null;
+
+            using (var httpClient = new HttpClient())
+            using (var formDataContent = new MultipartFormDataContent())
+            {
+                BuildMultipartFormData(dto, formDataContent);
+                _authorizationHandler.AddTokenBearer(session, httpClient);
+                msg = await httpClient.PostAsync(url, formDataContent);
+            }
+
+            return msg;
+        }
+
+
+
+        public override async Task<HttpResponseMessage> CreatePutAsync(string url,
+                                                              ChildActivityDTO dto,
+                                                              ISession session)
+        {
+            HttpResponseMessage msg = null;
+
+            using (var httpClient = new HttpClient())
+            using (var formDataContent = new MultipartFormDataContent())
+            {
+                BuildMultipartFormData(dto, formDataContent);
+                _authorizationHandler.AddTokenBearer(session, httpClient);
+                msg = await httpClient.PutAsync(url, formDataContent);
+            }
+
+            return msg;
+        }
+
+
+        private void BuildMultipartFormData(ChildActivityDTO dto,
+                                            MultipartFormDataContent formDataContent)
+        {
+            if (dto.ID > 0)
+            {
+                formDataContent.Add(new StringContent(dto.ID.ToString()), "ID");
+            }
+
+            formDataContent.Add(new StringContent(dto.Name), "Name");
+            formDataContent.Add(new StringContent(dto.Description), "Description");
+
+            int counter = 0;
+            foreach (var a in dto.Awards)
+            {
+                if (a.ID > 0)
+                {
+                    formDataContent.Add(new StringContent(a.ID.ToString()), "Awards[" + counter + "].ID");
+                }
+
+                formDataContent.Add(new StringContent(a.Name), "Awards[" + counter + "].Name");
+                formDataContent.Add(new StringContent(a.Description), "Awards[" + counter + "].Description");
+                formDataContent.Add(new StringContent(a.Date.ToString()), "Awards[" + counter + "].Date");
+
+                counter++;
+
+            }
+        }
+    }
+}
